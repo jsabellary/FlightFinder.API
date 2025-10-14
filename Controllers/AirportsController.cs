556703d2a5163ef3cc71f301e8ab1b87a55a588e
Judson ABLE BELLARY <judson_sumanth@yahoo.co.in>
2025-10-14 19:26:46 +0100

    Initial project files

---
+++ Controllers/AirportsController.cs
@@ -0,0 +1,18 @@
+﻿using FlightFinder.API;
+using FlightFinder.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FlightFinder.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AirportsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Airport> Get()
+        {
+            return SampleData.Airports;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+
+namespace FlightFinder.API
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "application/octet-stream" });
+            });
+            // Optional: enable CORS for React app in production; configure origins as needed.
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseResponseCompression();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            // Allow all origins by default; tighten in production.
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+
+            app.UseEndpoints(endpoints =>
+            {
+                // Map attribute-routed controllers (e.g., /api/airports, /api/flightsearch)
+                endpoints.MapControllers();
+
+                // Optional simple root endpoint to avoid 404 at "/"
+                endpoints.MapGet("/", async context =>
+                {
+                    context.Response.ContentType = "text/plain";
+                    await context.Response.WriteAsync("FlightFinder API is running. Try GET /api/airports");
+                });
+            });
+        }
+    }
+}
